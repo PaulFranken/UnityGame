abd745a010c37adbee6e3d9c7672f8eb64fa6553
Xelixi <deraaf4@hotmail.com>
2015-07-16 20:44:36 +0200

    Deel 2 m8

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+	}
+}
